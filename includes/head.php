d8693514a599feff16951d39205b7cbefc8e825a
DenLahpai <den.lahpai@icloud.com>
2019-03-13 10:41:59 +0630

    minor fixes

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 <link rel="stylesheet" href="styles/styles.css">
-<!-- <link rel="Shortcut icon" href="../images/logo.png"/> -->
+<link rel="Shortcut icon" href="images/event_logo.jpg"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $page_title; ?></title>
